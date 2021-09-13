f7863565545488630e75b7bef528dac5161c9d86
HalynaZinkiv <90180544+HalynaZinkiv@users.noreply.github.com>
2021-09-13 07:08:33 +0300

    Add files via upload

---
+++ 11.py
@@ -0,0 +1,4 @@
+from random import *
+
+number = round(uniform(1,10),2)
+print(number)
---
+++ 13.py
@@ -0,0 +1,6 @@
+from math import *
+
+angle_in_degrees = float(input())
+angle_in_radians = radians(angle_in_degrees)
+sine = sin(angle_in_radians)
+print(sine)
