f771f6836d8f093859c27d048ae1799f5bdf06aa
StasHorda <90180430+StasHorda@users.noreply.github.com>
2021-09-13 09:13:01 +0300

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,1 @@
+print(f'mixed fraction: 3 5/8\nfloating point number:{3+5/8}\nan integer:{int(3+5/8)}')
---
+++ 3.py
@@ -0,0 +1,4 @@
+print(f'18.5° C is {round(18.5*1.8+32, 1)} °F')
+print(f'-4° C is {round(-4*1.8+32, 1)} °F')
+
+
